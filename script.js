b409bab6aafdb4b4ea7077b0f0459411bb0118e5
Maxim Konoval <frontendmax@gmail.com>
2019-06-28 15:05:40 +0300

    Initial commit. Add basic features (no design)

---
+++ script.js
@@ -0,0 +1,56 @@
+const boxes = [...document.querySelectorAll('.box')]
+const contents = [...document.querySelectorAll('.content')]
+
+const getProp = (el, prop) => Math.ceil(parseInt(window.getComputedStyle(el).getPropertyValue(prop)))
+const getWidth = el => Math.ceil(el.offsetWidth)
+const getRelativeWidth = el => Math.ceil(getProp(el, 'width') / getProp(el.parentNode, 'width') * 100)
+
+document.querySelector('.relativeWidth').textContent = `${getRelativeWidth(boxes[0])}%`
+
+const renderContentWidth = arr => arr.forEach(el => el.textContent = `Content width: ${getWidth(el)}px`)
+const renderStaticElements = arr => arr.forEach(el => el.insertAdjacentHTML('afterbegin', `
+  <span class="box-border">border-width: ${getProp(el, 'border-width')}px</span>
+  <span class="box-padding">padding: ${getProp(el, 'padding')}px</span>
+`))
+
+const results = [
+  box => `while expected box width: ${getRelativeWidth(box)}% of the parent element
+    (<span class="expectedBoxWidth">${getWidth(box.querySelector('.content'))}</span>px)`,
+  box => `matches with expected box width: ${getRelativeWidth(box)}% of the parent element
+    (<span class="expectedBoxWidth">${getWidth(box)}</span>px)`
+]
+
+const renderDescription = () => boxes.forEach((box, i) => box.insertAdjacentHTML('afterend', `
+  <p>
+    Total box width = <span class="totalBoxWidth">${getWidth(box)}</span>px, where
+  </p>
+  <p>
+    border: ${getProp(box, 'border-width') * 2}px (${getProp(box, 'border-width')}px &times; 2) +
+    padding: ${getProp(box, 'padding') * 2}px (${getProp(box, 'padding')}px &times; 2) +
+    content: <span class="contentWidth">${getWidth(contents[i])}</span>px,
+  </p>
+  <p>
+    ${results[i](box)}
+  </p>
+`))
+
+renderContentWidth(contents)
+renderStaticElements(boxes)
+renderDescription()
+
+const handleResize = () => {
+  renderContentWidth(contents)
+
+  boxes.forEach((box, i) => {
+    box.parentNode.querySelector('.totalBoxWidth').textContent = getWidth(box)
+    box.parentNode.querySelector('.contentWidth').textContent = getWidth(contents[i])
+    box.parentNode.querySelector('.expectedBoxWidth').textContent = i === 0 ? getWidth(contents[i]) : getWidth(box)
+  })
+}
+
+window.addEventListener('resize', handleResize)
+
+// TODO: Place on GitHub
+// TODO: Calculate border and padding value positions in styles
+// TODO: Add value user inputs
+// TODO: Update with request animation frame?
