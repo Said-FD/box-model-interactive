8823adb64b57ec5178e0fac3f5eaffeb6eda310c
Maxim Konoval <frontendmax@gmail.com>
2019-07-13 23:13:16 +0300

    Implement new design. Refactor js

--- script.js
+++ script.js
@@ -1,66 +1,49 @@
 const root = document.documentElement
-const boxes = [...document.querySelectorAll('.box')]
 const contents = [...document.querySelectorAll('.content')]
+const descs = [...document.querySelectorAll('.description')]
 const inputs = [...document.querySelectorAll('input')]
-const [inputBorder, inputPadding] = inputs
+const [inputWidth, inputPadding, inputBorder] = inputs
 
-const getProp = (el, prop) => Math.ceil(parseInt(window.getComputedStyle(el).getPropertyValue(prop)))
-const getWidth = el => Math.ceil(el.offsetWidth)
-const getRelativeWidth = el => Math.ceil(getProp(el, 'width') / getProp(el.parentNode, 'width') * 100)
+let width = +inputWidth.value
+let padding = +inputPadding.value * 2
+let border = +inputBorder.value * 2
 
-document.querySelector('.relativeWidth').textContent = `${getRelativeWidth(boxes[0])}%`
+const getWidth = el => +el.offsetWidth
 
-const renderContentWidth = arr => arr.forEach(el => el.textContent = `Content width: ${getWidth(el)}px`)
-const renderStaticElements = arr => arr.forEach(el => el.insertAdjacentHTML('afterbegin', `
-  <span class="border-width">border-width: ${inputBorder.value}px</span>
-  <span class="padding">padding: ${inputPadding.value}px</span>
+contents.forEach(el => el.insertAdjacentHTML('beforeend', `
+  <span class="content-width" data-medium>${getWidth(el)}px</span>
 `))
 
-const results = [
-  box => `while expected box width: ${getRelativeWidth(box)}% of the parent element
-    (<span class="expectedBoxWidth">${getWidth(box.querySelector('.content'))}</span>px)`,
-  box => `matches the expected box width: ${getRelativeWidth(box)}% of the parent element
-    (<span class="expectedBoxWidth">${getWidth(box)}</span>px)`
-]
+descs[0].insertAdjacentHTML('beforeend', `
+  <p data-medium><span class="calc-total">${width + padding + border}</span>px = <span class="width-desc">${width}</span>px + <span class="padding-desc">${padding}</span>px + <span class="border-width-desc">${border}</span>px</p>
+`)
 
-const renderDescription = () => boxes.forEach((box, i) => box.insertAdjacentHTML('afterend', `
-  <p>
-    Total box width = <span class="totalBoxWidth">${getWidth(box)}</span>px, where
-  </p>
-  <p>
-    border: <span class="border-width-desc">${inputBorder.value * 2}px (${inputBorder.value}px</span> &times; 2) +
-    padding: <span class="padding-desc">${inputPadding.value * 2}px (${inputPadding.value}px</span> &times; 2) +
-    content: <span class="contentWidth">${getWidth(contents[i])}</span>px,
-  </p>
-  <p>
-    ${results[i](box)}
-  </p>
-`))
+descs[1].insertAdjacentHTML('beforeend', `
+  <p data-medium><span class="calc-content">${width - padding - border}</span>px = <span class="width-desc">${width}</span>px - <span class="padding-desc">${padding}</span>px - <span class="border-width-desc">${border}</span>px</p>
+`)
 
-renderContentWidth(contents)
-renderStaticElements(boxes)
-renderDescription()
-
-const handleResize = () => {
-  renderContentWidth(contents)
-
-  boxes.forEach((box, i) => {
-    box.parentNode.querySelector('.totalBoxWidth').textContent = getWidth(box)
-    box.parentNode.querySelector('.contentWidth').textContent = getWidth(contents[i])
-    box.parentNode.querySelector('.expectedBoxWidth').textContent = i === 0 ? getWidth(contents[i]) : getWidth(box)
-  })
-}
+const contentsWidth = [...document.querySelectorAll('.content-width')]
 
 const handleUserInput = function() {
-  root.style.setProperty(`--${this.name}`, this.value)
+  const name = this.name
+  const value = +this.value
 
-  document.querySelectorAll(`.${this.name}`).forEach(el => el.textContent = `${this.name}: ${this.value}px`)
-  document.querySelectorAll(`.${this.name}-desc`).forEach(el => el.textContent = `${this.value * 2}px (${this.value}px`)
+  root.style.setProperty(`--${name}`, value)
 
-  handleResize()
+  name === 'width' ? width = value :
+  name === 'padding' ? padding = value * 2 :
+  border = value * 2
+
+  document.querySelectorAll(`.${name}-desc`).forEach(el => {
+    el.textContent = name === 'width' ? width : value * 2
+  })
+
+  document.querySelector('.calc-total').textContent = width + padding + border
+  document.querySelector('.calc-content').textContent = width - padding - border
+
+  contentsWidth[0].textContent = `${width}px`
+  contentsWidth[1].textContent = `${width - padding - border}px`
 }
 
-window.addEventListener('resize', handleResize)
 inputs.forEach(input => input.addEventListener('input', handleUserInput))
-
-// TODO: Update with request animation frame?
+inputs.forEach(input => input.addEventListener('change', handleUserInput))
